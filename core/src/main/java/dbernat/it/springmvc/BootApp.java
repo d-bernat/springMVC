7ffc29cf3e4f9af9e1a9de4de5409f28b49acfa3
d-bernat <mail@it-bernat.de>
2020-10-06 22:43:43 +0200

    second initial

---
+++ core/src/main/java/dbernat/it/springmvc/BootApp.java
@@ -0,0 +1,15 @@
+package dbernat.it.springmvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BootApp {
+
+    private static final Logger log = LoggerFactory.getLogger(BootApp.class);
+
+    public static void main(String[] args)
+    {
+        log.info("hi there");
+    }
+
+}
